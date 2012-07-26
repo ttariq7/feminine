e59e792d93615c12d0ff7eef17df4e2fe4d31d66
ttariq7 <ttariq7@gmail.com>
2012-07-26 13:53:04 +0100

    next

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,28 @@
 Feminine::Application.routes.draw do
+  
+  root to: 'static_pages#home'
+  
+  get "static_pages/home"
+  get "static_pages/help"
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
