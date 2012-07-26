7fb9f094bc67874d26e3bd3cb5d3ca7164917664
ttariq7 <ttariq7@gmail.com>
2012-07-26 11:31:50 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Feminine::Application.config.secret_token = 'f48588bf4c35ab35c295a062ee62946a0c0ab870ec3bd379d49d608519f8bd01159b0ea69e8c6587be439a59b514b5636a5ed010a7414aa0e5037136a8e8ff7f'
