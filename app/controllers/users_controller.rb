235f2bccbd26f5572298daf6cb8ae6a4116434ad
ttariq7 <ttariq7@gmail.com>
2012-07-30 18:48:21 +0100

    signin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -44,8 +44,9 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render json: @user, status: :created, location: @user }
+        sign_in @user
+        flash[:success] = "Welcome to the Sample App!"
+        redirect_to @user
       else
         format.html { render action: "new" }
         format.json { render json: @user.errors, status: :unprocessable_entity }
