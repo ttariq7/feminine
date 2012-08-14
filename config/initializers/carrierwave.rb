ec342e4eca10766f300c145b7474907010157e90
ttariq7 <ttariq7@gmail.com>
2012-08-14 15:04:44 +0100

    awsuic

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,10 @@
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    :provider               => 'AWS',       # required
+    :aws_access_key_id      => 'AKIAJMIR27U4DN7S4JSQ',       # required
+    :aws_secret_access_key  => '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',       # required
+
+  }
+  config.fog_directory  = 'femynyn_images'                     # required
+
+end
