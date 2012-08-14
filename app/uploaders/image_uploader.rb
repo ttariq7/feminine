bfe27b0e88b7a8b0576e95e7039a54f812649a27
ttariq7 <ttariq7@gmail.com>
2012-08-14 15:42:25 +0100

    oiuvgjvhkbjlbuvg

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -17,6 +17,10 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
 
+  
+  def cache_dir
+  "#{Rails.root}/tmp/uploads"
+  end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
