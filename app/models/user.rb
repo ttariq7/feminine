4247c1457061489e10f16df13ffcf6a19a099d61
ttariq7 <ttariq7@gmail.com>
2012-09-09 16:08:29 +0100

    34rtvf

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ActiveRecord::Base
                                    class_name:  "Relationship",
                                    dependent:   :destroy
   has_many :followers, through: :reverse_relationships, source: :follower
-  has_many :comments
+  has_many :comments, dependent: :destroy
   
   before_save { |user| user.email = email.downcase }
   before_save :create_remember_token
