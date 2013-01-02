e7f27e680bac4f6818f394b194c63610fa6c1229
ttariq7 <ttariq7@gmail.com>
2013-01-02 13:34:40 +0000

    q4fervg

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,7 +19,7 @@ class User < ActiveRecord::Base
   before_save { |user| user.email = email.downcase }
   before_save :create_remember_token
 
-  validates :name, presence: true, length: { maximum: 9 }
+  validates :name, presence: true, length: { maximum: 9 }, uniqueness: { case_sensitive: false }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence:   true,
                     format:     { with: VALID_EMAIL_REGEX },
